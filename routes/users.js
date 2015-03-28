d3ecd78e163eed6dd0809df0d0b28ef75aab2c62
Mats Lundström <mats.a.lundstrom@gmail.com>
2015-03-28 01:15:54 +0100

    Beginning of a REST API.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res) {
 
-  res.json({message: 'respond with a resource'});
+    res.json({message: 'respond with a resource'});
+});
+
+router.delete('/', function (req, res) {
+
+    res.send('delete');
+});
+
+router.put('/', function (req, res) {
+
+    res.send('put');
+});
+
+router.post('/', function (req, res) {
+
+    res.send('post');
 });
 
 module.exports = router;
